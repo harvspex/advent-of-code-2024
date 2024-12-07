e919f6530f1d836d57276e03b4a8022a3d46a2c8
harvspex <harvspex@protonmail.com>
2024-12-07 16:20:00 +1100

    contains bug

--- aoc_2024/days/day_2.py
+++ aoc_2024/days/day_2.py
@@ -2,38 +2,64 @@ from aoc_2024.day_class.day import Day
 
 class Day2(Day):
     def part_1(self):
+        return self.get_safe_reports(remove_mode=False)
+
+    def part_2(self):
+        # 450 too low
+        # 458 too low
+        return self.get_safe_reports(remove_mode=True)
+
+    def get_safe_reports(self, remove_mode: bool=False):
         total_safe_reports: int =0
 
         for report in self.get_input():
-            if self.check_gradient(report):
+            if self.check_gradient(report, remove_mode=remove_mode):
                 total_safe_reports += 1
 
         return total_safe_reports
 
     @staticmethod
-    def check_gradient(report: list[int], min_g: int=1, max_g: int=3):
-        is_increasing: bool=None
+    def check_gradient(
+        report: list[int],
+        min_g: int=1,
+        max_g: int=3,
+        remove_mode: bool=False
+    ):
+        is_safe: bool = True
+        is_increasing: bool = None
 
         for i in range(len(report) - 1):
-            # Get difference of s 2 contiguous list items
+            # Get gradient of 2 contiguous list items
             current_g: int = report[i] - report[i+1]
 
-            # Determine if increasing or decreasing
-            if (is_increasing is None) and (current_g != 0):
+            # Gradient cannot be 0
+            if current_g == 0:
+                is_safe = False
+            # Set increasing or decreasing
+            elif is_increasing is None:
                 is_increasing = current_g > 0
-            # Return false if change in gradient direction
+            # Check if change in gradient direction
             elif (current_g > 0) != is_increasing:
-                return False
+                is_safe = False
 
             # Check if absolute value of gradient is within allowed limits
             if not (min_g <= abs(current_g) <= max_g):
-                return False
+                is_safe = False
 
-        # All gradients within allowed limits
-        return True
+            if not is_safe:
+                break
 
-    def part_2(self):
-        return None
+        if remove_mode and not is_safe:
+            return (
+                Day2.check_gradient(Day2.remove_element(report, i), min_g, max_g, remove_mode=False)
+                or Day2.check_gradient(Day2.remove_element(report, i+1), min_g, max_g, remove_mode=False)
+            )
+        else:
+            return is_safe
+
+    @staticmethod
+    def remove_element(lst: list, i: int):
+        return lst[:i] + lst[i+1:]
 
     def get_input(self):
         for line in super().get_input():
