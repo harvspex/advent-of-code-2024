f2f66b5e724bb2f364eb8c1978f42fe9125ea7a8
harvspex <harvspex@protonmail.com>
2024-12-08 16:19:14 +1100

    started part 2

--- aoc_2024/days/day_4.py
+++ aoc_2024/days/day_4.py
@@ -8,6 +8,18 @@ class Day4(Day):
     num_cols: int = None
     num_rows: int = None
 
+    def get_input(self):
+        for line in super().get_input():
+            row = [ch for ch in line.strip()]
+            if row:
+                self.crossword.append(row)
+            if self.num_cols is None:
+                self.num_cols = len(row)
+            elif self.num_cols != len(row):
+                raise ValueError('Error: row lengths do not match')
+
+        self.num_rows = len(self.crossword)
+
     def part_1(self):
         self.get_input()
         return self.check_all()
@@ -76,16 +88,36 @@ class Day4(Day):
         return total
 
     def part_2(self):
-        pass
+        self.get_input()
+        target = 'MAS'
 
-    def get_input(self):
-        for line in super().get_input():
-            row = [ch for ch in line.strip()]
-            if row:
-                self.crossword.append(row)
-            if self.num_cols is None:
-                self.num_cols = len(row)
-            elif self.num_cols != len(row):
-                raise ValueError('Error: row lengths do not match')
+    def yield_intersections(self, target: str):
+        len_target = len(target)
+        if len_target % 2 == 0:
+            raise ValueError('Target word cannot have even number of letters.')
 
-        self.num_rows = len(self.crossword)
+        middle_index: int = len_target // 2
+        middle_letter: str = target[middle_index]
+        remainder: int = len(target[:middle_index])
+
+        for row_i in range(remainder, self.num_rows-remainder):
+            for col_i in range(remainder, self.num_cols-remainder):
+                if self.crossword[row_i][col_i] == middle_letter:
+                    yield (row_i, col_i)
+
+    def check_for_mas(self, coords: tuple[int, int], target='MAS'):
+        # Checks 4 diagonal coordinates surrounding the provided coords tuple.
+        # Inflexible = only works for target of length 3
+        # Could be improved
+        r_target = ''.join(reversed(target))
+        
+        self.get_mas_substring(coords) 
+
+    def get_mas_substring(self, coords: tuple[int, int], reverse: bool=False):
+        coords_shifts = [(1,1),(0,0),(-1,-1)] if reverse else [(-1,1),(0,0),(1,-1)]
+
+        substring = ''
+        x, y = coords
+
+        for x_shift, y_shift in coords_shifts:
+            substring += self.crossword[x+x_shift][y+y_shift]
